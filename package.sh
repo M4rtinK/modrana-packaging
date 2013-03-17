df421081a053f9495f92285b56c717bc4993b5ab
Martin Kolman <martin.kolman@gmail.com>
2013-03-17 12:17:56 +0100

    actually this is 0.39.9

--- package.sh
+++ package.sh
@@ -7,7 +7,7 @@
 name=modrana
 version='0'
 minor='39'
-build='8'
+build='9'
 
 separator="."
 obs_package_path="home:MartinK:${name}/${name}/" ## Harmattan package OBS path
@@ -25,7 +25,54 @@ echo ${short_version_string} > ${name}/version
 ## "EOF"
 
 changelog=$( cat <<EOF
-- remember zoomlevel in QML GUI
+- massive map layer update ! :)
+ - new OpenStreetMap layers
+  - Mapnik b/w
+  - Landscape
+  - no labels
+  - labels (en)
+  - Hike and Bike
+  - OpenTopoMap
+  - Land Shading
+ - CloudMade Layers
+  - The Original
+  - Fine Line
+  - Red Alert
+  - Midnight Commander
+  - Fresh
+  - No-Names (shows unnamed roads and streats in OSM)
+  - Pale Dawn
+  - Tourist
+  - Blackout
+  - Thin
+  - Cycle Walk
+ - CloudMade 2x
+  - same layers, double-sized text and roads
+  - good for high-DPI screens
+ - Freemap.sk
+  - autoatlas
+  - touristic
+  - cyklomap
+  - skimap
+  - public transit
+ - new Google layers
+  - traffic
+  - traffic overlay
+  - traffic overlay labeled
+  - public transit
+  - weather Clesius
+  - weather Fahrenheit
+  - terrain
+  - terrain only (no labels)
+ - Czech layers
+  - amapy Tourist layer
+ - OpenSignal (mobile networg coverage)
+  - all overlay
+ - Yandex
+  - maps
+  - satellite
+  - overlay
+- added new coordinate tile coordinate substitution method
 EOF
 )
 
