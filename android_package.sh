3af4a1a13a9e6ecf448be0dd1a4298c0d5eac39d
Martin Kolman <martin.kolman@gmail.com>
2013-05-04 00:06:58 +0200

    Add script for Android packaging

---
+++ android_package.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Android packaging script
+# TODO: merge this with the main script
+
+PROJECT_FOLDER="modrana/android/android-project"
+VERSION_STRING=`cat modrana/version`
+TOPLEVEL=`pwd`
+INPUT_APK="modrana-debug.apk"
+OUTPUT_FOLDER="archive/android"
+
+echo "* updating from Git"
+cd modrana-git
+git pull
+cd $TOPLEVEL
+
+echo "* rsyncing and filtering"
+rsync -avzsh --delete --delete-excluded --progress modrana-git/ ${PROJECT_FOLDER}/app/modrana/  --exclude-from 'exclude-list.txt'
+
+echo "* byte compiling"
+python -m compileall -f ${PROJECT_FOLDER}/app/modrana -q
+
+echo "* removing .py files"
+find ${PROJECT_FOLDER}/app/modrana -type f -name "*.py" -exec rm -f {} \;
+
+echo "* updating version file"
+cp modrana/version ${PROJECT_FOLDER}/app/modrana/version.txt
+
+echo "* making Android package"
+cd ${PROJECT_FOLDER}
+#pydroid deploy complete
+cd $TOPLEVEL
+
+echo "* extracting modRana package"
+cp ${PROJECT_FOLDER}/android/bin/${INPUT_APK} ${OUTPUT_FOLDER}/modrana_${VERSION_STRING}.apk
+
+echo "* Android packaging done"
