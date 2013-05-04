031004192b91588ebab276fec7647da7ff0da0a4
Martin Kolman <martin.kolman@gmail.com>
2013-05-04 23:20:49 +0200

    0.40.3

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='40'
-export APP_VERSION_BUILD='2'
+export APP_VERSION_BUILD='3'
 
 ## add changelog on the lines after
 ## "APP_CHANGELOG=$( cat <<EOF"
@@ -14,7 +14,13 @@ export APP_VERSION_BUILD='2'
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- add new coordinate substitution method for layers using quadtree/quadkey addressing
-- add the VE UKOS layer
+- GTK GUI: potentially faster automatic & batch tile download
+ - and other activities, such as online lookups, that use threads
+ - looks like gobject.init_threads() and gtk.init_threads() was not called before gtk.main()
+ - as result, Python threads might have run only when some GTK events happened
+ - this would explain why modRana sometimes downloaded tiles slower with blanked screen
+- QML GUI: new nested map layer selection dialog that shows all layers
+ - it now shares with the GTK GUI the same data model
+ - as result, both GUI now should show the same list of layers
 EOF
 )
