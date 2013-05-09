70f32cb0d9f6da8e5d26bca3d8ba7e74eee7bc58
Martin Kolman <martin.kolman@gmail.com>
2013-05-10 01:06:48 +0200

    0.41.2

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='41'
-export APP_VERSION_BUILD='1'
+export APP_VERSION_BUILD='2'
 
 ## add changelog on the lines after
 ## "APP_CHANGELOG=$( cat <<EOF"
@@ -14,26 +14,11 @@ export APP_VERSION_BUILD='1'
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- modRana is now Python 3 compatible
- - Python 2.5 compatibility was of course preserved
- - as a result, modRana still works just fine with Python 2.5 on the N900
- - Python 3 compatibility should enable packaging modRana for BlackBerry 10
-- Android compatibility
- - modRana with the QML GUI now works on Android
- - Android device module has been added
- - installable APKs are available
- - APK generation script was added to the modRana packaging scripts
- - map data are stored in /sdcard/modrana/maps
-- QML GUI improvements
- - theme switching support
- - night theme for the QML GUI
- - the menu button can now show current mode, as in GTK GUI
- - thanks to Wikiwide for the idea & icons ! :)
- - finally some buttons in Options (related to theme switching & menu icon configuration)
- - fixed centering to the middle of the Atlantic at startup (center on Brno instead :) )
- - fixed main map icon now shows only one map layer
-- automatic tile downloading in GTK GUI should now be faster due to connection reuse provided by Urllib 3
-- new POI icon
-- various fixes
+- fix modRana not showing monav routing data packs
+- if map folder path is redirected by config file option, the monav routing data folder is also using the redirected path
+- new option for disabling (almost) all animations in QML GUI
+ - the option is in Options->UI
+ - when enabled, page switching becomes ridiculously fast, even on the N900 :)
+ - dialog animations are still ON, as there doesn't seem to be a clear way of switching them OFF
 EOF
 )
