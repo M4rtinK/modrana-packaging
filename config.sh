3b6cf6c4ca4170ca7aa01fac0092b18e66c400b7
Martin Kolman <martin.kolman@gmail.com>
2013-10-01 01:44:57 +0200

    0.43.4

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='43'
-export APP_VERSION_BUILD='3'
+export APP_VERSION_BUILD='4'
 
 ## add changelog on the lines after
 ## "APP_CHANGELOG=$( cat <<EOF"
@@ -14,30 +14,11 @@ export APP_VERSION_BUILD='3'
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- don't crash if a filesystem path can't be created
-- fix online routing not respecting route parameters
- - current mode is now correctly taken into account
- - "avoid highways" and "avoid toll roads" works correctly again
-- convert Wikipedia search to the new asynchronous provider framework
-- convert local search to the new asynchronous provider framework
-- address, wikipedia and local search wait for Internet connectivity
- - and enable it if needed
-- local search also waits for GPS, if needed
- - and will enable it, provided it is not turned of in options
-- local search now intializes GPS and Internet in parallel
- - this should speed up local search triggered from CLI
- - provided GPS and Internet is not yet initialized when triggered
-- CLI search debugging option
- - disables stdout suppression
-- add (hopefully) cross platform connectivity checking
-- the "search" button in the Fremantle app menu on the N900 now goes to the main search page
-- the modRana standard output now contains current Python version during startup
- - for easier debugging of Python version sepcific issues
-- fix routing to local search results
-- make "clear results" work for Address and Wikipedia search results
-- make "clear all" in the main search menu clear all results
-- tile storage type can now be selected in the QML GUI
- - in Options->Map
-- add a directions fix from Geoff Kuenning - thanks! :)
+- fix python-protobuf dependency
+ - looks newer version is needed for Monav routing server
+- only import Monav support once actually needed
+ - should shorten startup a little bit
+ - in case Monav support fails to load, the route module should still load fine
+- use conic based connectivity detection on Fremantle
 EOF
 )
