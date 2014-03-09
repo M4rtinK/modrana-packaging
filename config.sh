6c24731daaf0bd1abd3ca6576f149ed055f0a6da
Martin Kolman <martin.kolman@gmail.com>
2014-03-09 23:02:05 +0100

    V0.45.5

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='45'
-export APP_VERSION_BUILD='4'
+export APP_VERSION_BUILD='5'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,7 +17,8 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- fix theme color parsing on GTK GUI
-  - looks like the Silica theme broke it :)
+- fix speed display page in Qt 5/Sailfish GUI
+- show distance from current position for all search results in Qt 5/Sailfish GUI
+- show scroll decorators for most pages in Sailfish GUI
 EOF
 )
