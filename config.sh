255b245e2a5587391a22c26e018e2eede805ddfe
Martin Kolman <martin.kolman@gmail.com>
2014-04-17 00:35:50 +0200

    0.45.7

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='45'
-export APP_VERSION_BUILD='5'
+export APP_VERSION_BUILD='7'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,8 +17,6 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- fix speed display page in Qt 5/Sailfish GUI
-- show distance from current position for all search results in Qt 5/Sailfish GUI
-- show scroll decorators for most pages in Sailfish GUI
+- fix the Map1.eu and mtbmap layers not showing up in the map layer menu
 EOF
 )
