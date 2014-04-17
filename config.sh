1aa6b4b0264a741fe0251dd2a100ac4aad5576e5
Martin Kolman <martin.kolman@gmail.com>
2014-04-17 19:56:09 +0200

    0.46.1

--- config.sh
+++ config.sh
@@ -5,8 +5,8 @@
 
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
-export APP_VERSION_MINOR='45'
-export APP_VERSION_BUILD='7'
+export APP_VERSION_MINOR='46'
+export APP_VERSION_BUILD='1'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,6 +17,13 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- fix the Map1.eu and mtbmap layers not showing up in the map layer menu
+- local search works again thanks to a patch provided by Geoff Kuenning - thanks a lot ! :)
+- it is now possible to set local search radius in the GTK GUI (in options and directly in the search menu with a toggle button)
+- map display and dragging should now be faster
+- automatic map tile downloading has been rewritten and should be now more efficient
+- batch tile download has been rewritten and improved - should be now more efficient and faster
+- the tile hnadling code is now in much better shape overall
+- the batch tile menu in GTK GUI no longer lags and has a more logical structure
+- fix tile downloading not working in Sailfish GUI due to Python 3.4 breaking old bundled version of Urllib 3
 EOF
 )
