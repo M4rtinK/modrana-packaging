ed2bd944f962b6aac878d5ff93b0d78bcf7c034a
Martin Kolman <martin.kolman@gmail.com>
2014-04-17 22:41:56 +0200

    0.46.3

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='46'
-export APP_VERSION_BUILD='2'
+export APP_VERSION_BUILD='3'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,6 +17,6 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- fix tile URL generation for one of the providers
+- disable tile id debugging in pinchmap that was left on by mistake
 EOF
 )
