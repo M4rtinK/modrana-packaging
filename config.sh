68d1ffd1c4ae07b5b1acdf775a3d228d7620e033
Martin Kolman <martin.kolman@gmail.com>
2014-07-29 00:06:27 +0200

    O.48.1

--- config.sh
+++ config.sh
@@ -5,8 +5,8 @@
 
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
-export APP_VERSION_MINOR='47'
-export APP_VERSION_BUILD='2'
+export APP_VERSION_MINOR='48'
+export APP_VERSION_BUILD='1'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,9 +17,11 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- add local search support to the Qt 5 GUI
-- sort local search results by initial distance in Qt 5 GUI
-- show search results on the map in Qt 5 GUI
-- highlight the result that was selected in result list in red on map in Qt 5 GUI
+- added persistent overlay configuration support in Qt 5 GUI
+- aproximate batch download size is now displayed during batch download in GTK GUI
+- location debuging support in Qt 5 GUI
+- fixed zoom level not being saved in Qt 5 GUI
+- fixed current track logging settings not being displayed properly in GTK GUI
+- fixed URL for the Map1.eu map layer
 EOF
 )
