04a04a657921f80b1885e2eebd220ad424323368
Martin Kolman <martin.kolman@gmail.com>
2014-08-02 00:25:49 +0200

    0.48.2

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='48'
-export APP_VERSION_BUILD='1'
+export APP_VERSION_BUILD='2'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,11 +17,6 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- added persistent overlay configuration support in Qt 5 GUI
-- aproximate batch download size is now displayed during batch download in GTK GUI
-- location debuging support in Qt 5 GUI
-- fixed zoom level not being saved in Qt 5 GUI
-- fixed current track logging settings not being displayed properly in GTK GUI
-- fixed URL for the Map1.eu map layer
+- add a missing module to the Sailfish OS package
 EOF
 )
