5259cca7c56229532e57231af743d2da3b9e468d
Martin Kolman <martin.kolman@gmail.com>
2014-10-18 21:50:21 +0200

    0.49.3

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='49'
-export APP_VERSION_BUILD='2'
+export APP_VERSION_BUILD='3'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,6 +17,7 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- remove a file breaking build
+- add a map screen compass rose
+ - it can be turned on/off and its opacity changed in Options->UI
 EOF
 )
