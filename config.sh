657d95e1440a5ca1aa0742887c444d19e8091ca3
Martin Kolman <martin.kolman@gmail.com>
2014-11-16 00:52:37 +0100

    0.49.12

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='49'
-export APP_VERSION_BUILD='7'
+export APP_VERSION_BUILD='12'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,8 +17,5 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-- fix the compass rose not showing up in the Qt 5 GUI on Sailfish OS
-- fix a typo preventing location startup if no last known position exists in Qt 5 GUI
-- fix a race condition that could prevent some map tiles from working properly in Qt 5 GUI
 EOF
 )
