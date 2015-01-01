2d7e7fc9cec62fed7361ad30127930d3aa89a085
Martin Kolman <martin.kolman@gmail.com>
2015-01-01 22:25:23 +0100

    0.50.2

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='50'
-export APP_VERSION_BUILD='1'
+export APP_VERSION_BUILD='2'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,16 +17,8 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-* layer timeout support (Frederic Ferner)
- * dynamic map layers (traffic, weather, etc.) now automatically load new tiles once the oled ones time out
- * not yet handles staring at the same part of map for hours
- * you need to move the map a bit so that the tiles are requested again & refetched
-* support for setting network usage mode in Qt 5 GUI (Frederic Ferner)
- * max - unlimited network usage
- * minimal - no automatic tile download
-* Thanks a lot to Frederic for these nice two patches! :)
-* fix text sizing on icon grid
-* fix compass rose display with qrc (Android)
- * it still does not rotate on Android some reason :)
+* first official modRana release for Android! :)
+* make theme parsing more robust
+* fix window sizing regression on Sailfish OS
 EOF
 )
