0b9ec45d32186d6dd9d57200f6e6109f8851ac4c
Martin Kolman <martin.kolman@gmail.com>
2015-01-02 03:03:30 +0100

    0.50.3

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='50'
-export APP_VERSION_BUILD='2'
+export APP_VERSION_BUILD='3'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,8 +17,6 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-* first official modRana release for Android! :)
-* make theme parsing more robust
-* fix window sizing regression on Sailfish OS
+* Python 2.5 compatibility fixes
 EOF
 )
