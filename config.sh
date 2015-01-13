1adb2fc518201182865377ef2e4da1a1dae03c6a
Martin Kolman <martin.kolman@gmail.com>
2015-01-13 23:42:04 +0100

    0.50.5

--- config.sh
+++ config.sh
@@ -6,7 +6,7 @@
 export APP_NAME='modrana'
 export APP_VERSION_MAIN='0'
 export APP_VERSION_MINOR='50'
-export APP_VERSION_BUILD='4'
+export APP_VERSION_BUILD='5'
 
 export LOG_FOLDER_NAME=build_logs
 export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
@@ -17,6 +17,12 @@ export LOG_FOLDER=${APP_NAME}/${LOG_FOLDER_NAME}/
 ## "EOF"
 
 export APP_CHANGELOG=$( cat <<EOF
-* the actual Python 2.5 compatibility fixes
+- make it possible to show the back button on Sailfish OS
+ - can be enabled from Options->UI
+- fix Sqlite tile storage with Qt 5 GUI
+- fix the Animate switch to work correctly on Sailfish OS
+- fix a few page push transitions that have not respected the animation setting in Qt 5 GUI
+- fix back button in the detail menu of a POI restored on startup breaking the GTK GUI
+- use proper logging during batch tile download 
 EOF
 )
